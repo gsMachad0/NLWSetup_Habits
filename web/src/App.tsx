4241a19b0a0013b32485d75cc908168c7b4469fb
Guilherme Saldanha Machado <guilherme.saldanha.machado@gmail.com>
2023-01-17 07:54:42 -0300

    Initial Commit
    Repository creation

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3}/>
+    </div>
+  )
+}
+
+export default App
+
+// Componente: Reaproveitar / Isolar
+// Propriedade: Informação enviada para modificar um componente visual ou comportamentalmente
